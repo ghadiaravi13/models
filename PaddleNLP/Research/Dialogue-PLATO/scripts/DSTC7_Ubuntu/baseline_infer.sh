45879e51273e09e815ae73f76d420367e7fdc5c2
Ravi Ghadia <40660742+ghadiaravi13@users.noreply.github.com>
2022-04-07 15:17:58 +0530

    added bs arg

--- PaddleNLP/Research/Dialogue-PLATO/scripts/DSTC7_Ubuntu/baseline_infer.sh
+++ PaddleNLP/Research/Dialogue-PLATO/scripts/DSTC7_Ubuntu/baseline_infer.sh
@@ -6,6 +6,8 @@ VOCAB_PATH=model-baseline/Bert/vocab.txt
 DATA_DIR=data/DSTC7_Ubuntu
 INIT_CHECKPOINT=outputs/DSTC7_Ubuntu.baseline/best.model
 DATA_TYPE=multi
+BATCH_SIZE=$1
+# preferred bs is 48
 
 # CUDA environment settings.
 export CUDA_VISIBLE_DEVICES=0
@@ -27,7 +29,7 @@ python -u \
     --vocab_path $VOCAB_PATH \
     --data_dir $DATA_DIR \
     --data_type $DATA_TYPE \
-    --batch_size 48 \
+    --batch_size $BATCH_SIZE \
     --num_latent 0 \
     --num_type_embeddings 2 \
     --init_checkpoint $INIT_CHECKPOINT \
